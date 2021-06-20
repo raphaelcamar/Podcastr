7dea98afd8470f91285f68ad67cf08aea46d9c95
raphaelcamar <raphaelcamar@outlook.com>
2021-06-20 17:45:00 -0300

    Cleaning

--- podcastr-next/src/pages/_app.tsx
+++ podcastr-next/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import Header from '../components/Header';
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
 import { Player } from '../components/Player';
-import { PlayerContext, PlayerContextProvider } from '../contexts/PlayerContext';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
